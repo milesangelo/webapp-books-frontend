e4dadaa4421ad64384fcf2ea854b13e2065d352f
Miles Vendetti <mvendetti@chathamfinancial.com>
2022-05-24 11:24:58 -0600

    Adds drawer navigation with a few default items

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,64 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import { AppBar, Box, Button, IconButton, Toolbar, Typography } from '@mui/material';
-import MenuIcon  from '@mui/icons-material/Menu';
+import React, { useState } from "react";
+import "./App.css";
+import {
+  AppBar,
+  Box,
+  Button,
+  Drawer,
+  IconButton,
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemIcon,
+  ListItemText,
+  Toolbar,
+  Typography,
+} from "@mui/material";
+import MenuIcon from "@mui/icons-material/Menu";
+import MailIcon from "@mui/icons-material/Mail";
+import InboxIcon from "@mui/icons-material/Inbox";
 
 function App() {
+  // Drawer open state.
+  const [drawerState, setDrawerState] = useState(false);
+
+  // Open/ close the drawer.
+  const toggleDrawer =
+    (open: boolean) => (event: React.KeyboardEvent | React.MouseEvent) => {
+      if (
+        event.type === "keydown" &&
+        ((event as React.KeyboardEvent).key === "Tab" ||
+          (event as React.KeyboardEvent).key === "Shift")
+      ) {
+        return;
+      }
+
+      setDrawerState(open);
+    };
+
+  // Generate the items in the menu drawer.
+  const list = () => (
+    <Box
+      sx={{ width: 250 }}
+      role="presentation"
+      onClick={toggleDrawer(false)}
+      onKeyDown={toggleDrawer(false)}
+    >
+      <List>
+        {["My Books", "Starred", "About Me"].map((text, index) => (
+          <ListItem key={text} disablePadding>
+            <ListItemButton>
+              <ListItemIcon>
+                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+              </ListItemIcon>
+              <ListItemText primary={text} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+    </Box>
+  );
+
   return (
     <Box sx={{ flexGrow: 1 }}>
       <AppBar position="static">
@@ -15,9 +69,17 @@ function App() {
             color="inherit"
             aria-label="menu"
             sx={{ mr: 2 }}
+            onClick={toggleDrawer(true)}
           >
             <MenuIcon />
           </IconButton>
+          <Drawer
+            anchor="left"
+            open={drawerState}
+            onClose={toggleDrawer(false)}
+          >
+            {list()}
+          </Drawer>
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
             Books
           </Typography>
