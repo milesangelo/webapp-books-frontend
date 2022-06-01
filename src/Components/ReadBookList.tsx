c6c055bfeca55008f024bc22b715666bcf74e391
milesangelo <milesvendetti@gmail.com>
2022-05-31 19:57:53 -0600

    Adds bookcard list to component

--- src/Components/ReadBookList.tsx
+++ src/Components/ReadBookList.tsx
@@ -1,103 +1,103 @@
-import * as React from 'react';
-import Box from '@mui/material/Box';
-import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
-import CardContent from '@mui/material/CardContent';
-import Button from '@mui/material/Button';
-import Typography from '@mui/material/Typography';
-import { useState } from 'react';
-import { Container, Grid, Rating } from '@mui/material';
-import { maxWidth } from '@mui/system';
+import * as React from "react";
+import Box from "@mui/material/Box";
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+import { useState } from "react";
+import { Container, Grid, Rating } from "@mui/material";
+import { maxWidth } from "@mui/system";
+import { BookSharp } from "@mui/icons-material";
 
-// const bull = (
-//   <Box
-//     component="span"
-//     sx={{ display: 'inline-block', mx: '2px', transform: 'scale(0.8)' }}
-//   >
-//     •
-//   </Box>
-// );
-
-
-const BookCard = () => {
-
-    const [value, setValue] = useState(1.0);
-    return(
-        <Box sx={{
-            padding: 5.0,
-            display: 'inline-flex'
-        }}>
-            <CardContent sx={{
-                bgcolor: 'primary.light',
-                minWidth: 200
-            }}>
-                <Typography variant='h5' color='primary.contrastText' component='div' gutterBottom>
-                    Book Title
-                </Typography>
-                <Typography variant='h6' color='primary.contrastText'>
-                    Author
-                </Typography>
-                <Typography color='text.secondary' sx={{  mb: 0.1 }} variant='body2'>
-                    Rating:
-                </Typography>
-                <Rating 
-                    name='simple-controlled' 
-                    value={value} 
-                    onChange={( event, value ) => {
-                        setValue(value || 0.0);
-                    }}
-                />
-            </CardContent>
-        </Box>
-    )
+interface Book {
+  title: string;
+  author: string;
+  rating: number;
 }
 
-const ReadBookList = () => { 
+interface BookCardProps {
+  title: string;
+  author: string;
+  rating: number;
+}
 
-    return(
-        <Grid direction="row" >
-        {BookCard()}
-        
-        {BookCard()}
-        {BookCard()}
-        
-        </Grid>
-        
-)};
+const BookCard = (props: BookCardProps): JSX.Element => {
+  const [value, setValue] = useState(0.0);
+  return (
+    <>
+      <CardContent
+        sx={{
+          bgcolor: "primary.light",
+        }}
+      >
+        <Typography
+          variant="h5"
+          color="primary.contrastText"
+          component="div"
+          gutterBottom
+        >
+          {props.title}
+        </Typography>
+        <Typography variant="h6" color="primary.contrastText">
+          {props.author}
+        </Typography>
+        <Typography color="text.secondary" sx={{ mb: 0.1 }} variant="body2">
+          Rating:
+        </Typography>
+        <Rating
+          name="simple-controlled"
+          value={props.rating}
+          onChange={(event, value) => {
+            setValue(value || 0.0);
+          }}
+        />
+      </CardContent>
+    </>
+  );
+};
+
+const ReadBookList = () => {
+  const [books, setBooks] = useState([{
+    title: '',
+    author: '',
+    rating: 0
+  }]);
+
+  React.useEffect(() => {
+    setBooks([{
+      title: 'test1', 
+      author: 'author1', 
+      rating: 0.0 
+    }, {
+      title: 'test2', 
+      author: 'author2', 
+      rating: 1.0
+    }, {
+      title: 'test3', 
+      author: 'author3', 
+      rating: 3.5
+    }, {
+      title: 'test4', 
+      author: 'author4', 
+      rating: 5.0
+    }
+    ])
+  }, []);
+  return (
+    <Grid
+      container
+      sx={{ paddingLeft: 4, paddingTop: 4, paddingRight: 4 }}
+      spacing={4}
+    >
+      {books &&
+        books.map((item, i) => (
+          <Grid key={i} item sx={{ minWidth: 240 }} xs={4}>
+            <BookCard title={item.title} author={item.author} rating={item.rating} />
+          </Grid>
+        ))}
+    </Grid>
+  );
+};
 
 export default ReadBookList;
-
-
-
-
-// const card = (
-//   <React.Fragment>
-//     <CardContent>
-//       <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
-//         Word of the Day
-//       </Typography>
-//       <Typography variant="h5" component="div">
-//         be{bull}nev{bull}o{bull}lent
-//       </Typography>
-//       <Typography sx={{ mb: 1.5 }} color="text.secondary">
-//         adjective
-//       </Typography>
-//       <Typography variant="body2">
-//         well meaning and kindly.
-//         <br />
-//         {'"a benevolent smile"'}
-//       </Typography>
-//     </CardContent>
-//     <CardActions>
-//       <Button size="small">Learn More</Button>
-//     </CardActions>
-//   </React.Fragment>
-// );
-
-// export default function OutlinedCard() {
-//   return (
-//     <Box sx={{ minWidth: 275 }}>
-//       <Card variant="outlined">{card}</Card>
-//     </Box>
-//   );
-// }
