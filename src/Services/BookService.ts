6d4ea6c2cfbb192aa608817e9b76b49fd3d17151
milesangelo <milesvendetti@gmail.com>
2022-06-17 22:39:49 -0600

    Adds creation and getting of books

--- src/Services/BookService.ts
+++ src/Services/BookService.ts
@@ -1,6 +1,7 @@
-import axios from '../Api/axios';
-import { Book } from '../Components/ReadBookList';
-import { getErrorMessage } from '../Errors';
+import { Error } from "@mui/icons-material";
+import axios from "../Api/axios";
+import { Book } from "../Components/ReadBookList";
+import { getErrorMessage } from "../Errors";
 
 class BookService {
     url = "http://books-api.milesangelo.io/api/books";
@@ -8,12 +9,34 @@ class BookService {
     getAll = async () => {
         try {
             const response = await axios.get(this.url, {
-                    headers: { 'Content-Type': 'application/json' },
-                }
-            );
-            return (JSON.parse(response?.data) as Book[])
+                headers: { "Content-Type": "application/json" },
+            });
+            return JSON.parse(response?.data) as Book[];
         } catch (err) {
-            reportError({ message: getErrorMessage(err) })
+            reportError({ message: getErrorMessage(err) });
+        }
+    };
+
+    getAllBooks = async (token: string | undefined) => {
+        if (!token) {
+            console.error('token undefined')
+            return undefined;
+        }
+        
+        try {
+            const response = await fetch(this.url.concat('/getAllBooks'), {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify({
+                    jwt: token,
+                }),
+            });
+            const data = await response.text();
+            return JSON.parse(data) as Book[];
+        } catch (err) {
+            reportError({ message: getErrorMessage(err) });
         }
     };
 }
